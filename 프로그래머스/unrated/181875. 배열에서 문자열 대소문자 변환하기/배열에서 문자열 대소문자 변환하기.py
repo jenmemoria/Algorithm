daf9738fb3a255c4dc1e5e6a920e5cbccfbce08c
제은 <147487137+jenmemoria@users.noreply.github.com>
2023-11-06 12:40:49 +0900

    [level unrated] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -0,0 +1,10 @@
+def solution(strArr):
+    answer = []
+    
+    for i,j in enumerate(strArr):
+        if(i % 2 == 0):
+            answer.append(j.lower())
+        else:
+            answer.append(j.upper())
+    
+    return answer
